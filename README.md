99aff2cf34703db7840033fbf384000f4c6b1d7e
Michael Yakovlev <kvadrofilii@gmail.com>
2023-10-04 22:41:49 +0300

    init: first version plugin

---
+++ README.md
@@ -0,0 +1,45 @@
+# eslint-plugin-fsd
+
+test eslint plugin
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-fsd`:
+
+```sh
+npm install @kvadrofilii/eslint-plugin-fsd --save-dev
+```
+
+## Usage
+
+Add `eslint-plugin-fsd` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+  "plugins": ["fsd"]
+}
+```
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+  "rules": {
+    "fsd/rule-name": 2
+  }
+}
+```
+
+## Rules
+
+<!-- begin auto-generated rules list -->
+
+TODO: Run eslint-doc-generator to generate the rules list.
+
+<!-- end auto-generated rules list -->
---
+++ tests/lib/rules/path-checker.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview FSD relative path checker
+ * @author Michael Yakovlev
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/path-checker'),
+  RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run('path-checker', rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
+
+  invalid: [
+    {
+      code: 'problem',
+      errors: [{ message: 'Fill me in.', type: 'Me too' }],
+    },
+  ],
+});
